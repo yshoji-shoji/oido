3cdb0a5b87a4a292c72f057e476eed78f26a2080
Yu Shoji <shoji.yu@moneyforward.co.jp>
2024-05-16 22:25:48 +0900

    Added random filter

--- js/app.js
+++ js/app.js
@@ -1,9 +1,27 @@
+const random = (max) =>
+  Math.floor(Math.random() * (Math.ceil(max) + Math.ceil(0)) + Math.ceil(0));
+
 const init = () => {
-  const initialCount = Math.floor(
-    Math.random() * (Math.ceil(1000) + Math.ceil(0)) + Math.ceil(0)
-  );
+  const initialCount = random(1000);
   const likeCountDisplay = window.document.getElementById("like-count-display");
   likeCountDisplay.innerText = initialCount.toString();
+  filter();
+};
+
+const filter = () => {
+  const filterPatterns = [
+    "filter: blur(8px);",
+    "filter: contrast(400%);",
+    "filter: grayscale(80%);",
+    "filter: hue-rotate(90deg);",
+    "filter: drop-shadow(16px 16px 20px red) invert(75%);",
+    "",
+  ];
+
+  const footer = window.document.getElementById("footer");
+
+  const filterIndex = random(6);
+  footer.style = filterPatterns[filterIndex];
 };
 
 const like = () => {
@@ -13,6 +31,7 @@ const like = () => {
       window.document.getElementById("like-count-display");
     const currentCount = parseInt(likeCountDisplay.innerText);
     likeCountDisplay.innerText = (currentCount + 1).toString();
+    filter();
   });
 };
 
