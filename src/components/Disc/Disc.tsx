ab097a2bc524636508cc029ebd975040c286150f
Yu Shoji <shoji.yu@moneyforward.co.jp>
2024-09-28 14:30:58 +0900

    Build

--- src/components/Disc/Disc.tsx
+++ src/components/Disc/Disc.tsx
@@ -5,33 +5,36 @@ export const Disc = () => {
   console.log(disks);
 
   return (
-    <div>
-      {disks.map((disk) => (
-        <div key={disk.title}>
-          <h3>{disk.title}</h3>
-          <div style={{ display: 'flex' }}>
-            {disk.image.map((img) => (
-              <a href={disk.link} target="_blank" rel="noopener noreferrer">
-                <img
-                  src={img.url}
-                  style={{ width: '45%', marginRight: '2%' }}
-                />
-              </a>
-            ))}
-          </div>
+    <>
+      <h2>Discography</h2>
+      <div>
+        {disks.map((disk) => (
+          <div key={disk.title}>
+            <h3>{disk.title}</h3>
+            <div style={{ display: 'flex' }}>
+              {disk.image.map((img) => (
+                <a href={disk.link} target="_blank" rel="noopener noreferrer">
+                  <img
+                    src={img.url}
+                    style={{ width: '45%', marginRight: '2%' }}
+                  />
+                </a>
+              ))}
+            </div>
 
-          <div style={{ marginTop: '20px' }}>
-            {disk.description?.split('\n').map((item) => (
-              <p style={{ margin: '2px' }}>{item}</p>
-            ))}
+            <div style={{ marginTop: '20px' }}>
+              {disk.description?.split('\n').map((item) => (
+                <p style={{ margin: '2px' }}>{item}</p>
+              ))}
+            </div>
+            {disk.link && (
+              <a href={disk.link} target="_blank" rel="noopener noreferrer">
+                詳細を見る
+              </a>
+            )}
           </div>
-          {disk.link && (
-            <a href={disk.link} target="_blank" rel="noopener noreferrer">
-              詳細を見る
-            </a>
-          )}
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </>
   );
 };
